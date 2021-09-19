0058b5e651d6e3831005055bd0e01834a6a19714
RomikMakavana <makavanaromik1214@gmail.com>
2021-09-19 11:13:32 +0530

    Change Enviroment Url

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 let config = {
     app_code: "cover-compared",
     cache_time: 300,
-    api_url: "http://localhost:3006/"
+    api_url: "http://localhost:3006/api/"
 }
 
 if (process.env.NODE_ENV && process.env.NODE_ENV == 'staging') {
-    config.api_url = "";
+    config.api_url = "https://staging-covercompared.polkacover.com/api/";
 }else if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
-    config.api_url = "";
+    config.api_url = "https://covercompared.polkacover.com/api/";
 }
 
 module.exports = config;
