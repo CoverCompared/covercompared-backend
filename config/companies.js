53a4803565497881918c927b64f5c75233d9c7a1
RomikMakavana <makavanaromik1214@gmail.com>
2021-12-06 16:27:50 +0530

    Config staging url for insureace api

--- config/companies.js
+++ config/companies.js
@@ -64,20 +64,20 @@ module.exports = {
         "status": true,
         "min_eth": 0.5,
         "logo_url": "https://app.insurace.io/asset/product/",
-        "access_code": "BIJtkcwZVbqksdkGQphamIi6yXfUpd2cwIxeDs6jmT4uXYaWJwONIA==",
+        "access_code": config.insurace.access_code,
         "owner_id": "0x48D88a5D10595338E46C82256858BaCbcD38e224",
         "apis": {
             "cover_list": {
-                "url": "https://api.insurace.io/ops/v1/getProductList"
+                "url": `${config.insurace.base_url}getProductList`
             },
             "cover_quote": {
-                "url": `https://api.insurace.io/ops/v1/getCoverPremium`
+                "url": `${config.insurace.base_url}getCoverPremium`
             },
             "confirm_premium": {
-                "url": `https://api.insurace.io/ops/v1/confirmCoverPremium`
+                "url": `${config.insurace.base_url}confirmCoverPremium`
             },
             "currency_list": {
-                "url": `https://api.insurace.io/ops/v1/getCurrencyList`
+                "url": `${config.insurace.base_url}getCurrencyList`
             }
         }
     },
--- config/index.js
+++ config/index.js
@@ -12,8 +12,18 @@ let config = {
 
 if (process.env.NODE_ENV && process.env.NODE_ENV == 'staging') {
     config.api_url = "https://staging-covercompared.polkacover.com/api/";
-}else if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
+} else if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
     config.api_url = "https://covercompared.polkacover.com/api/";
+}
+
+config.insurace = {
+    base_url: "https://insurace-sl-microservice.azurewebsites.net/",
+    access_code: "VXS3K8XdLj1Gzr1xX2JtiQSi5fBphwBraEEMCmYbO26o2Ebv7lQUJQ=="
+}
+
+if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
+    config.insurace.base_url = "https://api.insurace.io/ops/v1/";
+    config.insurace.access_code = "BIJtkcwZVbqksdkGQphamIi6yXfUpd2cwIxeDs6jmT4uXYaWJwONIA==";
 }
 
 module.exports = config;
