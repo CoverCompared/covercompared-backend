51e727c4767d42b5f78c9b6cc94461718a78742b
Romik Makavana <makavanaromik1214@gmail.com>
2022-03-29 15:15:21 +0530

    Insurace issue change

--- libs/cron.js
+++ libs/cron.js
@@ -11,25 +11,25 @@ const web3Actions = async () => {
         console.log("Web3 Connect");
         let web3 = await web3Connect.connect();
 
-        if (!(process.env.MSO_SYNC_TRANSACTIONS_OFF && process.env.MSO_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.mso.msoPolicySync();
-        else console.log("MSO  WEB3 Sync Is Off");
+        // if (!(process.env.MSO_SYNC_TRANSACTIONS_OFF && process.env.MSO_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.mso.msoPolicySync();
+        // else console.log("MSO  WEB3 Sync Is Off");
 
-        if (!(process.env.P4L_SYNC_TRANSACTIONS_OFF && process.env.P4L_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.p4l.p4lPolicySync();
-        else console.log("P4L  WEB3 Sync Is Off");
+        // if (!(process.env.P4L_SYNC_TRANSACTIONS_OFF && process.env.P4L_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.p4l.p4lPolicySync();
+        // else console.log("P4L  WEB3 Sync Is Off");
 
-        if (!(process.env.INSURACE_SYNC_TRANSACTIONS_OFF && process.env.INSURACE_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.insurace.policySync();
-        else console.log("INSURACE  WEB3 Sync Is Off");
+        // if (!(process.env.INSURACE_SYNC_TRANSACTIONS_OFF && process.env.INSURACE_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.insurace.policySync();
+        // else console.log("INSURACE  WEB3 Sync Is Off");
 
-        if (!(process.env.NEXUS_SYNC_TRANSACTIONS_OFF && process.env.NEXUS_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.nexus.policySync();
-        else console.log("NEXUS  WEB3 Sync Is Off");
+        // if (!(process.env.NEXUS_SYNC_TRANSACTIONS_OFF && process.env.NEXUS_SYNC_TRANSACTIONS_OFF == "1")) await web3Connect.smart_contracts.nexus.policySync();
+        // else console.log("NEXUS  WEB3 Sync Is Off");
 
     } catch (error) {
         /**
          * Send Error Report : issue wile run cron job
          */
         await helpers.addErrorReport(
-            "issue", 
-            "Issue while run cron job", 
+            "issue",
+            "Issue while run cron job",
             { errorNote: error.toString(), error }
         )
     }
@@ -45,11 +45,15 @@ const cron = async () => {
     console.log("Check Smart Contract Address...");
 
     web3Actions();
-    
+
     NodeCron.schedule('*/2 * * * * *', async () => {
-        console.log("TEST Working", moment().format("HH:mm:ss"));
+        try {
+            console.log("TEST Working", moment().format("HH:mm:ss"));
+        } catch (error) {
+            console.log("Cron Error ", error);
+        }
     })
-    
+
     console.log("Cron running a every 30 minute");
     NodeCron.schedule('*/30 * * * *', async () => {
         try {
